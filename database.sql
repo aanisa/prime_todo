da733d4be8b27cc01a13b2e0eb54017abd8bb0e0
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-27 11:24:02 -0500

    statement to order tasks by completion status

--- database.sql
+++ database.sql
@@ -17,3 +17,6 @@ DELETE FROM "tasks" WHERE "id"= 1;
 
 --update task status
 UPDATE "tasks" SET "status" = TRUE WHERE "id"= 7;
+
+--put completed tasks in bottom
+SELECT * FROM "tasks" ORDER BY status ASC;
